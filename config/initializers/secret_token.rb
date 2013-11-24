d90f21ce318f1820a16316a228a87603f3517c24
Knott <wittawat@cmu.edu>
2013-11-24 12:49:41 -0800

    RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TfaCorp::Application.config.secret_key_base = '2820b23e2a7912968ee0d2fc1f209e2bb7090cb153eea7cfdcc69ed07022023508a248e10c5ce68375ba020e29bc475bb6b921efe01538fbdc41b7af0ae3e48b'
