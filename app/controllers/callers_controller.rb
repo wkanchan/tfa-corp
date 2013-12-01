eecab75f2cd597c22a9b39dbe48b179a201304a7
Knott <wittawat@cmu.edu>
2013-11-30 18:56:28 -0800

    fix

--- app/controllers/callers_controller.rb
+++ app/controllers/callers_controller.rb
@@ -18,44 +18,23 @@ class CallersController < ApplicationController
   end
 
   def sign_in_2
-=begin
-    # Check parameters
-    if params[:user_id].nil? or params[:otp].nil?
-      @result = "{ result: 0, message: 'Require user_id and otp parameters' }"
-      return
+    # Do the sign in
+    uri = URI.parse(Rails.application.config.tfa_server_uri+"/users/sign_in")
+    query = {
+      corp_id: Rails.application.config.corp_id,
+      otp: CGI::escape(params[:otp])
+    }
+    response = Net::HTTP.post_form(uri, query)
+    if response.code != "200"
+      @result['result'] = 0
+      @result['message'] = 'Could not connect to 2nd factor authenticator.'
+    else
+      @result = response.body.to_json
     end
 
-    # Check that OTP is valid
-    corp_key = Rails.application.config.corp_key
-    begin
-      plain = AES.decrypt(params[:otp], corp_key)
-    rescue
-      @result = "{ result: 0, message: 'Invalid OTP' }"
-      return
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @result }
     end
-    if plain[0] != params[:user_id]
-      @result = "{ result: 0, message: 'Given OTP is not associated with this user_id' }"
-      return
-    end
-=end
-
-  # Do the sign in
-  uri = URI.parse(Rails.application.config.tfa_server_uri+"/users/sign_in")
-  query = {
-    corp_id: Rails.application.config.corp_id,
-    otp: CGI::escape(params[:otp])
-  }
-  response = Net::HTTP.post_form(uri, query)
-  if response.code != "200"
-    @result.result = 0
-    @result.message = 'Could not connect to 2nd factor authenticator.'
-  else
-    @result = response.body.to_json
-  end
-
-  respond_to do |format|
-    format.html # index.html.erb
-    format.json { render json: @result }
   end
-
 end
