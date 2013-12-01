d0cc9e893b8ff8a89d19303523cbe29b2ab0ac43
Knott <wittawat@cmu.edu>
2013-11-30 18:46:39 -0800

    fix

--- app/controllers/callers_controller.rb
+++ app/controllers/callers_controller.rb
@@ -18,26 +18,6 @@ class CallersController < ApplicationController
   end
 
   def sign_in_2
-=begin
-    # Check parameters
-    if params[:user_id].nil? or params[:otp].nil?
-      @result = "{ result: 0, message: 'Require user_id and otp parameters' }"
-      return
-    end
-
-    # Check that OTP is valid
-    corp_key = Rails.application.config.corp_key
-    begin
-      plain = AES.decrypt(params[:otp], corp_key)
-    rescue
-      @result = "{ result: 0, message: 'Invalid OTP' }"
-      return
-    end
-    if plain[0] != params[:user_id]
-      @result = "{ result: 0, message: 'Given OTP is not associated with this user_id' }"
-      return
-    end
-=end
 
     # Do the sign in
     uri = URI.parse(Rails.application.config.tfa_server_uri+"/users/sign_in")
