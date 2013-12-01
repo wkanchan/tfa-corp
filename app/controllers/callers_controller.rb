e06d35f01ca79636cafe02c90fd0e6a7093fd9cc
Knott <wittawat@cmu.edu>
2013-11-30 19:31:08 -0800

    Change response to JSON

--- app/controllers/callers_controller.rb
+++ app/controllers/callers_controller.rb
@@ -46,10 +46,17 @@ class CallersController < ApplicationController
       otp: CGI::escape(params[:otp])
     }
     response = Net::HTTP.post_form(uri, query)
+    puts response.body.as_json
     if response.code != "200"
-      @result = "{ result: 0, message: 'Could not connect to 2nd factor authenticator.' }"
+      @result.result = 0
+      @result.message = 'Could not connect to 2nd factor authenticator.'
     else
-      @result = response.body
+      @result = response.body.as_json
+    end
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @result }
     end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,5 +29,5 @@ TfaCorp::Application.configure do
 
   config.corp_id = "1"
   config.corp_key = "31e083a5c06d4cf9e4ee2a426f3438c8"
-  config.tfa_server_uri = "http://localhost:3000"
+  config.tfa_server_uri = "http://localhost:3001"
 end
