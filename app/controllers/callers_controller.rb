162ba19196a1d7f66900742058de298dc6d5116d
Knott <wittawat@cmu.edu>
2013-12-02 02:15:59 -0800

    Add user check

--- app/controllers/callers_controller.rb
+++ app/controllers/callers_controller.rb
@@ -3,19 +3,27 @@ class CallersController < ApplicationController
   require "uri"
 
   def secondauth
-    # Do the sign in
+    # URI for 2nd authen
     uri = URI.parse(Rails.application.config.tfa_server_uri+"/users/sign_in")
-    query = {
-      corp_id: Rails.application.config.corp_id,
-      otp: CGI::escape(params[:otp])
-    }
-    response = Net::HTTP.post_form(uri, query)
-    puts response.body.as_json
-    if response.code != "200"
-      @result.result = 0
-      @result.message = 'Could not connect to 2nd factor authenticator.'
-    else
-      @result = response.body.as_json
+    # Get user ID from email
+    begin
+      user_id = User.where(email:params[:email]).first.id
+
+      # Got user ID from email. Prepare query to 2nd auth
+      query = {
+        user_id: user_id,
+        corp_id: Rails.application.config.corp_id,
+        otp: CGI::escape(params[:otp])
+      }
+      response = Net::HTTP.post_form(uri, query)
+      # puts response.body.as_json
+      if response.code != "200"
+        @result = { result: 0, message: 'Could not connect to 2nd factor authenticator.' }
+      else
+        @result = response.body.as_json
+      end
+    rescue
+      @result = { result: 0, message: 'Invalid user email' }
     end
 
     respond_to do |format|
